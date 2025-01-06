66aac70a9e42c3bb5dcd1746176454e163936a98
Lokesh Nalamasa <nlokeshgoud7@gmail.com>
2025-01-06 13:42:08 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,46 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+
+// SIGNUP Functionality
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    if (
+      !username ||
+      !email ||
+      !password ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      return res.status(400).json({
+        message: "All fields are required",
+        success: false,
+      });
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Email already exists",
+        success: false,
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(200).json({
+      message: "User created successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- api/index.js
+++ api/index.js
@@ -1,21 +1,26 @@
 
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from "./routes/user.route.js"
+import authRoutes from './routes/auth.route.js';
+import connectDB from './db/dbConnection.js';
 
 const app =  express();
 dotenv.config();
 
+//middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log("MongoDB is connected")}).catch(err=>{
-        console.log(err);
-    })
-app.use('/api/user', userRoutes)
 
+//routes
+
+app.use('/api/user', userRoutes)
+app.use('/api/auth',authRoutes)
+
+
+//port and connection
 app.listen(3000,()=>{
-    console.log('server is listening to port 3000!');   
+    console.log('server is listening to port 3000!');  
+    connectDB(); 
 })
 
