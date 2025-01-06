da8e67e16d91ca73d465b531d8c0a38895f301af
Lokesh Nalamasa <nlokeshgoud7@gmail.com>
2025-01-06 18:40:15 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-
+import jwt from "jsonwebtoken";
 
 // SIGNUP Functionality
 
@@ -45,4 +45,43 @@ export const signup = async (req, res) => {
   }
 };
 
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
 
+  if (!email || !password || email === "" || password === "") {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    const isValidPassword = await bcrypt.compare(password,user.password);
+    if (!isValidPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token =  jwt.sign({userId:user._id},process.env.JWT_SECRET,{expiresIn:"1d"});
+
+    const {password:pass,...rest} = user._doc;
+
+    res.status(200).cookie("access-token",token,{
+      httpOnly:true,
+      maxAge: 24 * 60 * 60 * 1000 ,// 1 day
+    }).json(rest)
+
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
