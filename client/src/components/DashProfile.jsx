e4b0cc4f8c2d3436c803c2ede090c5d9a83c1865
Lokesh Nalamasa <nlokeshgoud7@gmail.com>
2025-01-08 15:44:38 +0530

    add complete delete user account functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,4 +1,4 @@
-import { Alert, Button, TextInput } from "flowbite-react";
+import { Alert, Button, Modal, ModalBody, TextInput } from "flowbite-react";
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -14,24 +14,28 @@ import {
   updateStart,
   updateSuccess,
   updateFailure,
+  deleteUserFailure,
+  deleteUserSuccess,
+  deleteUserStart,
 } from "../redux/user/userSlice";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 const DashProfile = () => {
-  const { currentUser } = useSelector((state) => state.user);
-  //Adding images to google logo
+  const { currentUser, error } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
-
   const [imageFileUploadingProgress, setimageFileUploadingProgress] =
     useState(null);
   const [imageFileUploadingError, setimageFileUploadingError] = useState(null);
   const [imageFileUploading, setImageFileUploading] = useState(false);
   const [updateUserSuccess, setupdateUserSuccess] = useState(null);
   const [updateUserError, setupdateUserError] = useState(null);
-
   const [formData, setFormData] = useState({});
+  const [showModal, setShowModal] = useState(false);
+
   const dispatch = useDispatch();
   const filePickerRef = useRef();
+
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -44,6 +48,7 @@ const DashProfile = () => {
       uploadImage();
     }
   }, [imageFile]);
+
   const uploadImage = async () => {
     //  console.log('uploading image....');
     setImageFileUploading(true);
@@ -78,6 +83,7 @@ const DashProfile = () => {
       }
     );
   };
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -85,13 +91,13 @@ const DashProfile = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setupdateUserError(null);
-    setupdateUserSuccess(null)
+    setupdateUserSuccess(null);
     if (Object.keys(formData).length === 0) {
-      setupdateUserError('No changes made')
+      setupdateUserError("No changes made");
       return;
     }
     if (imageFileUploading) {
-      setupdateUserError('please wait for image to upload')
+      setupdateUserError("please wait for image to upload");
       return;
     }
     try {
@@ -108,7 +114,7 @@ const DashProfile = () => {
 
       if (!res.ok) {
         dispatch(updateFailure(data.messsage));
-        setupdateUserError(data.message)
+        setupdateUserError(data.message);
       } else {
         dispatch(updateSuccess(data));
         setupdateUserSuccess("User's profile updated successfully");
@@ -116,6 +122,22 @@ const DashProfile = () => {
     } catch (error) {
       dispatch(updateFailure(error.messsage));
     }
+  };
+
+  const handleDeleteUser = async () => {
+    setShowModal(false);
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = res.json();
+      if (!res.ok) {
+        dispatch(deleteUserFailure(data.message));
+      } else {
+        dispatch(deleteUserSuccess(data));
+      }
+    } catch (error) {}
   };
 
   return (
@@ -192,7 +214,9 @@ const DashProfile = () => {
         </Button>
       </form>
       <div className="text-red-500 flex justify-between mt-5">
-        <span className="cursor-pointer">Delete Account</span>
+        <span onClick={() => setShowModal(true)} className="cursor-pointer">
+          Delete Account
+        </span>
         <span className="cursor-pointer">Sign Out</span>
       </div>
       {updateUserSuccess && (
@@ -200,12 +224,38 @@ const DashProfile = () => {
           {updateUserSuccess}
         </Alert>
       )}
-      {updateUserError && (
-        <Alert color="failure" className="mt-5">
-          {updateUserError}
+      {error && (
+        <Alert color="success" className="mt-5">
+          {error}
         </Alert>
       )}
 
+      {
+        <Modal
+          show={showModal}
+          onClose={() => setShowModal(false)}
+          popup
+          size="md"
+        >
+          <Modal.Header />
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+              <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+                Are you sure you want to delete your account?
+              </h3>
+              <div className="flex justify-center gap-4">
+                <Button color="failure" onClick={handleDeleteUser}>
+                  Yes, I'm sure
+                </Button>
+                <Button color="gray" onClick={() => setShowModal(false)}>
+                  No, cancel
+                </Button>
+              </div>
+            </div>
+          </Modal.Body>
+        </Modal>
+      }
     </div>
   );
 };
--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 const PrivateRoute = () => {
 
     const {currentUser} = useSelector((state) => state.user);
-  return currentUser ? <Outlet/> : <Navigate to="/login"/>
+  return currentUser ? <Outlet/> : <Navigate to="/sign-in"/>
 }
 
 export default PrivateRoute
