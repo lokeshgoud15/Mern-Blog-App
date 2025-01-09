e219729d0f24f65d694334fe076875d3443d3c3e
Lokesh Nalamasa <nlokeshgoud7@gmail.com>
2025-01-09 21:20:42 +0530

    create get users api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -92,3 +92,48 @@ export const signout = async (req, res) => {
     res.status(500).json({ success: false, message: "Failed to sign out" });
   }
 };
+
+export const getusers = async (req, res) => {
+  if (!req.user.isAdmin) {
+    return res
+      .status(403)
+      .json({ message: "You are not allowed to see all users" });
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Failed to get all users" });
+    
+  }
+};
